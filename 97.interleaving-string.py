920daf0fab98c48ba9ee4330486bbf4fbb531ea7
loveonme <dengtiansheng@gmail.com>
2019-07-26 20:39:08 +0800

    97

--- 97.interleaving-string.py
+++ 97.interleaving-string.py
@@ -2,23 +2,31 @@ class Solution(object):
     def isInterleave(self, s1, s2, s3):
         '''
         a aa,b bb,a babab
-        
+        注意[[False]*3]*4，不可以这样初始化，会导致内存复用，改一个值会修改一大排，导致结果错误
         ''' 
         if len(s1) + len(s2) != len(s3):
             return False
-        dp = [[False]*(len(s1)+1)]*(len(s2)+1)
+        #dp = [[False]*(len(s1)+1)]*(len(s2)+1)
+        dp = []
+        for i in range(len(s2)+1):
+            res = []
+            for j in range(len(s1)+1):
+                res.append(False)
+            dp.append(res)
+
         dp[0][0] = True
         print dp
         for i in range(len(s1)):
             if s1[i] == s3[i]:
-                dp[i][0] = True
+                dp[i+1][0] = True
         for i in range(len(s2)):
             if s2[i] == s3[i]:
-                dp[0][j] = True        
+                dp[0][j+1] = True        
         print dp
         for i in range(1,len(s1)+1):
             for j in range(1,len(s2)+1):
                 dp[i][j] = (dp[i-1][j] and s1[i - 1] == s3[i+j-2]) or (dp[i][j-1] and s2[j - 1] == s3[i+j-2])
+                print dp[i-1][j],dp[i][j],dp[i][j-1]
         print dp
         return dp[len(s1)][len(s2)]
 
